d9b91df0d8f77b898bf3386c2911b111eb79b05d
Alex <alexiseliudrdz@gmail.com>
2016-07-28 11:46:26 -0500

    Basic structure sinatra

--- simple.rb
+++ simple.rb
@@ -1,10 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 class Simple < Sinatra::Base
-  
-        
-  get '/' do
-    "Hola mundo"      
-  end
+# set utf-8 for outgoing
+before do
+  headers "Content-Type" => "text/html; charset=utf-8"
+end
+
+get '/' do
+  @title = "Hello"
+  erb :form
+end
+
+post '/' do
+  @name = "#{params[:post][:first_name]} #{params[:post][:last_name]}"
+  @title = "Hello #{@name}"
+  erb :hello
+end
 
 end
+
